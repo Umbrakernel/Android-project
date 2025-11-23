15eac896ba30cb999ef1eae1166fcb8d407f0fc2
ilya <iliazh06@gmail.com>
2025-11-23 18:29:21 +0300

    Изменил калькулятор 2.1

--- app/src/main/java/com/example/android_project/CalculatorActivity.kt
+++ app/src/main/java/com/example/android_project/CalculatorActivity.kt
@@ -23,77 +23,125 @@ class CalculatorActivity : AppCompatActivity() {
 
         var one: Button = findViewById(R.id.button_one)
         one.setOnClickListener({
-            Display.setText("1")
+            if(Display.text == "0") {
+                Display.text = "1"
+            }
+            else {
+                Display.setText(Display.text.toString() + "1")
+            }
         })
 
         var two: Button = findViewById(R.id.button_two)
         two.setOnClickListener({
-            Display.setText("2")
+            if(Display.text == "0") {
+                Display.text = "2"
+            }
+            else {
+                Display.setText(Display.text.toString() + "2")
+            }
         })
 
         var three: Button = findViewById(R.id.button_three)
         three.setOnClickListener({
-            Display.setText("3")
+            if(Display.text == "0") {
+                Display.text = "3"
+            }
+            else {
+                Display.setText(Display.text.toString() + "3")
+            }
         })
 
         var four: Button = findViewById(R.id.button_four)
         four.setOnClickListener({
-            Display.setText("4")
+            if(Display.text == "0") {
+                Display.text = "4"
+            }
+            else {
+                Display.setText(Display.text.toString() + "4")
+            }
         })
 
         var five: Button = findViewById(R.id.button_five)
         five.setOnClickListener({
-            Display.setText("5")
+            if(Display.text == "0") {
+                Display.text = "5"
+            }
+            else {
+                Display.setText(Display.text.toString() + "5")
+            }
         })
 
         var six: Button = findViewById(R.id.button_six)
         six.setOnClickListener({
-            Display.setText("6")
+            if(Display.text == "0") {
+                Display.text = "6"
+            }
+            else {
+                Display.setText(Display.text.toString() + "6")
+            }
         })
 
         var seven: Button = findViewById(R.id.button_seven)
         seven.setOnClickListener({
-            Display.setText("7")
+            if(Display.text == "0") {
+                Display.text = "7"
+            }
+            else {
+                Display.setText(Display.text.toString() + "7")
+            }
         })
 
         var eight: Button = findViewById(R.id.button_eight)
         eight.setOnClickListener({
-            Display.setText("8")
+            if(Display.text == "0") {
+                Display.text = "8"
+            }
+            else {
+                Display.setText(Display.text.toString() + "8")
+            }
         })
 
         var nine: Button = findViewById(R.id.button_nine)
         nine.setOnClickListener({
-            Display.setText("9")
+            if(Display.text == "0") {
+                Display.text = "9"
+            }
+            else {
+                Display.setText(Display.text.toString() + "9")
+            }
         })
 
         var nulle: Button = findViewById(R.id.button_null)
         nulle.setOnClickListener({
-            Display.setText("0")
+            if(Display.text == "0") {
+                Display.text = "0"
+            }
+            Display.setText(Display.text.toString() + "0")
         })
 
         var plus: Button = findViewById(R.id.button_plus)
         plus.setOnClickListener({
-            Display.setText("+")
+            Display.setText(Display.text.toString() + "+")
         })
 
         var minus: Button = findViewById(R.id.button_minus)
         minus.setOnClickListener({
-            Display.setText("-")
+            Display.setText(Display.text.toString() + "-")
         })
 
         var multiply: Button = findViewById(R.id.button_multiply)
         multiply.setOnClickListener({
-            Display.setText("*")
+            Display.setText(Display.text.toString() + "*")
         })
 
         var divide: Button = findViewById(R.id.button_divide)
         divide.setOnClickListener({
-            Display.setText("/")
+            Display.setText(Display.text.toString() + "/")
         })
 
         var point: Button = findViewById(R.id.button_point)
         point.setOnClickListener({
-            Display.setText(".")
+            Display.setText(Display.text.toString() + ".")
         })
 
         var equal: Button = findViewById(R.id.button_equal)
@@ -112,10 +160,6 @@ class CalculatorActivity : AppCompatActivity() {
                 result = numbers[0].toDouble() * numbers[1].toDouble()
             } else if (text.contains("/")) {
                 val numbers = text.split("/")
-                if (numbers[1].toDouble() == 0.0) {
-                    Display.setText("Ошибка")
-                    return@setOnClickListener
-                }
                 result = numbers[0].toDouble() / numbers[1].toDouble()
             }
 
